cc72f248204586c6729f75414048b8fe903b9589
Coquierik <erikcoq@gmail.com>
2025-01-15 16:38:35 -0300

    first commit

---
+++ Saude-Beleza/script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset:true});
+
+
+sr.reveal('.grid-conteudo', {duration: 3000});
+sr.reveal('.video', {duration: 4000});
+sr.reveal('.grid-equipe', {rotate: {x: 0, y: 40, z: 0}, duration: 4000});
